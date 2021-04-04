d2b91b3d3111f4a9189899c5235f60593640c588
파랑이 <parangee9706@gmail.com>
2021-04-04 17:08:58 +0900

    feat: init layout component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,52 +4,53 @@ import React from 'react'
 import Authentication from '../../util/Authentication/Authentication'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import AppLayout from "./components/Layout";
 
-export default class App extends React.Component{
-    constructor(props){
+export default class App extends React.Component {
+    constructor(props) {
         super(props)
         this.Authentication = new Authentication()
 
         //if the extension is running on twitch or dev rig, set the shorthand here. otherwise, set to null. 
         this.twitch = window.Twitch ? window.Twitch.ext : null
-        this.state={
-            finishedLoading:false,
-            theme:'light',
-            isVisible:true
+        this.state = {
+            finishedLoading: false,
+            theme: 'light',
+            isVisible: true
         }
     }
 
-    contextUpdate(context, delta){
-        if(delta.includes('theme')){
-            this.setState(()=>{
-                return {theme:context.theme}
+    contextUpdate(context, delta) {
+        if (delta.includes('theme')) {
+            this.setState(() => {
+                return {theme: context.theme}
             })
         }
     }
 
-    visibilityChanged(isVisible){
-        this.setState(()=>{
+    visibilityChanged(isVisible) {
+        this.setState(() => {
             return {
                 isVisible
             }
         })
     }
 
-    componentDidMount(){
-        if(this.twitch){
-            this.twitch.onAuthorized((auth)=>{
+    componentDidMount() {
+        if (this.twitch) {
+            this.twitch.onAuthorized((auth) => {
                 this.Authentication.setToken(auth.token, auth.userId)
-                if(!this.state.finishedLoading){
+                if (!this.state.finishedLoading) {
                     // if the component hasn't finished loading (as in we've not set up after getting a token), let's set it up now.
 
                     // now we've done the setup for the component, let's set the state to true to force a rerender with the correct data.
-                    this.setState(()=>{
-                        return {finishedLoading:true}
+                    this.setState(() => {
+                        return {finishedLoading: true}
                     })
                 }
             })
 
-            this.twitch.listen('broadcast',(target,contentType,body)=>{
+            this.twitch.listen('broadcast', (target, contentType, body) => {
                 this.twitch.rig.log(`New PubSub message!\n${target}\n${contentType}\n${body}`)
                 // now that you've got a listener, do something with the result... 
 
@@ -57,36 +58,42 @@ export default class App extends React.Component{
 
             })
 
-            this.twitch.onVisibilityChanged((isVisible,_c)=>{
+            this.twitch.onVisibilityChanged((isVisible, _c) => {
                 this.visibilityChanged(isVisible)
             })
 
-            this.twitch.onContext((context,delta)=>{
-                this.contextUpdate(context,delta)
+            this.twitch.onContext((context, delta) => {
+                this.contextUpdate(context, delta)
             })
         }
     }
 
-    componentWillUnmount(){
-        if(this.twitch){
-            this.twitch.unlisten('broadcast', ()=>console.log('successfully unlistened'))
+    componentWillUnmount() {
+        if (this.twitch) {
+            this.twitch.unlisten('broadcast', () => console.log('successfully unlistened'))
         }
     }
-    
-    render(){
-        if(this.state.finishedLoading && this.state.isVisible){
+
+    render() {
+        if (this.state.finishedLoading && this.state.isVisible) {
             return (
                 <div className="App">
-                    <div className={this.state.theme === 'light' ? 'App-light' : 'App-dark'} >
-                        <p>Hello world!</p>
-                        <p>My token is: {this.Authentication.state.token}</p>
-                        <p>My opaque ID is {this.Authentication.getOpaqueId()}.</p>
-                        <div>{this.Authentication.isModerator() ? <p>I am currently a mod, and here's a special mod button <input value='mod button' type='button'/></p>  : 'I am currently not a mod.'}</div>
-                        <p>I have {this.Authentication.hasSharedId() ? `shared my ID, and my user_id is ${this.Authentication.getUserId()}` : 'not shared my ID'}.</p>
+                    <div className={this.state.theme === 'light' ? 'App-light' : 'App-dark'}>
+                        <AppLayout>
+                            {/*<p>Hello world!</p>*/}
+                            {/*<p>My token is: {this.Authentication.state.token}</p>*/}
+                            {/*<p>My opaque ID is {this.Authentication.getOpaqueId()}.</p>*/}
+                            {/*<div>{this.Authentication.isModerator() ?*/}
+                            {/*    <p>I am currently a mod, and here's a special mod button <input value='mod button'*/}
+                            {/*                                                                    type='button'/>*/}
+                            {/*    </p> : 'I am currently not a mod.'}</div>*/}
+                            {/*<p>I*/}
+                            {/*    have {this.Authentication.hasSharedId() ? `shared my ID, and my user_id is ${this.Authentication.getUserId()}` : 'not shared my ID'}.</p>*/}
+                        </AppLayout>
                     </div>
                 </div>
             )
-        }else{
+        } else {
             return (
                 <div className="App">
                 </div>
---
+++ src/components/App/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @type {React.FC}
+ */
+const AppLayout = ({children}) => {
+    return (
+        <div>
+            asdf
+            {children}
+        </div>
+    );
+};
+
+export default AppLayout;
